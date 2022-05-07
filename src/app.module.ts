7c244be3b2dce8382d44a4f0d5f9e8e0ed93bed3
Simon LUCIDO <simon.lucido@etu.umontpellier.fr>
2022-05-07 21:57:34 +0200

    fix(env): fix typo in env parsing
    - "DATABAASE_PASSWORD" -> "DATABASE_PASSWORD"
    
    Signed-off-by: Simon LUCIDO <simon.lucido@etu.umontpellier.fr>

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,7 @@ import { TokenModule } from './token/token.module';
       host: process.env['DATABASE_HOST'],
       port: parseInt(process.env['DATABASE_PORT']),
       username: process.env['DATABASE_USERNAME'],
-      password: process.env['DATAABASE_PASSWORD'] || '',
+      password: process.env['DATABASE_PASSWORD'] || '',
       database: process.env['DATABASE_DB'],
       autoLoadModels: true,
       synchronize: true,
